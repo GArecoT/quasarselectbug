535197563035872353b554b87813fb0e83cdf5d1
Gabriel Areco <areco23@gmail.com>
2024-09-24 16:27:57 -0400

    LaLiLuLeLo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <div class="q-pa-md">
+    <q-select
+      label="Select Teste"
+      v-model="select"
+      :options="options2"
+      dense
+      options-dense
+      option-label="nome"
+      use-input
+      @filter="
+        (val, update) => {
+          options2 = filtrar(val, update, options1, 'nome');
+        }
+      "
     >
-  </q-page>
+    </q-select>
+  </div>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+import { onMounted, ref, toRaw } from "vue";
+
+const options1 = ref([{nome: 'cerveja'},{nome:  'pinga'},{nome: 'cachaça'},{nome: 'óleo de borogodó'},{nome: 'cigarrinho do capeta'}])
+const options2 = ref(options1.value)
+
+ function filtrar(val, update, store, nome) {
+  let lista = structuredClone(toRaw(store));
+  if (val === "") {
+    update(() => {
+      return;
+    });
+
+    return lista;
+  }
+  update(() => {
+    const needle = val.toLowerCase();
+    lista = Object.values(
+      lista.filter(
+        (v) => v[nome].toLowerCase().indexOf(needle) > -1,
+      ),
+    );
+  });
+  return lista;
+}
+
+
 </script>
